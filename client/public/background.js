130206b49b8529eeacc2447eeb5ea1ed0c39323c
Hucksapp <hucksapp@gmail.com>
2024-11-14 08:51:06 -0800

    :thread:ui update

--- client/public/background.js
+++ client/public/background.js
@@ -39,7 +39,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     moveGroupToNewWindow(request.groupId);
     sendResponse({ status: "done" });
   } else if (request.action === "openNewTabInGroup") {
-    openNewTabInGroup(request.groupId)
+    openNewTabInGroup(request.groupId);
     sendResponse({ status: "done" });
   } else if (request.action === "refreshGroupCache") {
     CreateOrUpdateGroupCache();
